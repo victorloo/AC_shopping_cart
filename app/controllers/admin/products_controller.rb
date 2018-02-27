21f099b709d4ca61361b91c3603d58f9d202fa9d
victorloo <ila9970@gmail.com>
2018-02-28 03:14:53 +0800

    add restaurant index action and view for admin

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,6 @@
+class Admin::ProductsController < ApplicationController
+
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   root 'products#index'
 
+  namespace :admin do
+    root 'products#index'
+  end
 end
