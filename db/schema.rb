2301b140a1797e319c1b156ab82d2c2e05c9d95a
victorloo <ila9970@gmail.com>
2018-02-28 21:57:37 +0800

    create model cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227191753) do
+ActiveRecord::Schema.define(version: 20180228135642) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
