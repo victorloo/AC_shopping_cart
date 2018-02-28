9cba950356af9eb875b98d6026fff1cd65996287
victorloo <ila9970@gmail.com>
2018-02-28 22:50:18 +0800

    setup method of add product to cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :current_cart
+
+  private
+
+  def current_cart
+    @cart || set_cart
+    # return @cart if @cart exist, or call set_cart
+  end
+
+  def set_cart
+    
+    if session[:cart_id] 
+      @cart = Cart.find_by(id: session[:cart_id])
+    end
+
+    @cart ||= Cart.create
+
+    session[:cart_id] = @cart.id
+    @cart
+  end
 end
