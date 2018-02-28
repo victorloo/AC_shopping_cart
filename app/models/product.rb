cbcc9014114250f7535f0ddbb8904b2b42ba7fa2
victorloo <ila9970@gmail.com>
2018-02-28 22:25:14 +0800

    build relationship

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   validates_presence_of :name, :description, :price
 
   mount_uploader :image, PhotoUploader
+
+  has_many :cart_items, dependent: :destroy
+  has_many :carts, through: :cart_items
 end
