d31aaed1a020ce4a33d4e6ea9ebc3494c9667759
victorloo <ila9970@gmail.com>
2018-02-28 20:22:54 +0800

    setup products resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :products, only: [:index, :show]
   root 'products#index'
 
   namespace :admin do
