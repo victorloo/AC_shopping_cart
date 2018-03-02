dc4382dc8d4e198171f3060851be92a42e0424dc
victorloo <ila9970@gmail.com>
2018-03-03 02:29:01 +0800

    add new method: remove from cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,8 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.page(params[:page]).per(9)
+    session[:abc] = '123'
+    @products = Product.page(params[:page]).per(8)
   end
     
   def show
@@ -10,8 +11,21 @@ class ProductsController < ApplicationController
   
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_cart_item(@product)
+    current_cart.add_cart_item(@product) # 加 product item
 
     redirect_to root_path
+    # 也可以這樣寫︰redirect_back(fallback_location: root_path)
   end
+
+  # 名稱不用 delete ，是因為 delete 專門給 destroy 使用
+  def remove_from_cart
+    product = Product.find(params[:id])
+    # 懶人刪除法，全部刪除
+    cart_item = current_cart.cart_items.find_by(product_id: product)
+    cart_item.destroy
+
+    # 正確的數量+-要自己想
+    redirect_back(fallback_location: root_path)
+  end
+
 end
