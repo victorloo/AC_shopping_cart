f79da5675d45a2d00581e649cef00c01ae12dc3e
victorloo <ila9970@gmail.com>
2018-03-05 03:58:23 +0800

    set model relation

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,7 @@ class Product < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :carts, through: :cart_items
   # 因為連結關聯表，因此會有另一張間接的表，要標示出來(用through)
+
+  has_many :order_items
+  has_many :orders, through: :order_items
 end
