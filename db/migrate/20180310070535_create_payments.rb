2b58506b5e819569e28569de260b0bff15335b13
victorloo <ila9970@gmail.com>
2018-03-10 15:07:49 +0800

    create model payment

--- db/migrate/20180310070535_create_payments.rb
+++ db/migrate/20180310070535_create_payments.rb
@@ -1,4 +1,4 @@
-class Payment < ActiveRecord::Migration[5.1]
+class CreatePayments < ActiveRecord::Migration[5.1]
   def change
     create_table :payments do |t|
       t.integer :order_id, null: false
@@ -7,7 +7,7 @@ class Payment < ActiveRecord::Migration[5.1]
       t.string :payment_method
       t.datetime :paid_at
       t.text :params
-    
+
       t.timestamps
     end
   end
