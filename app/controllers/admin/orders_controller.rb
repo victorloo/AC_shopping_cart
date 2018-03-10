b83d5a69404a2c432c9f5cfda865e5fa565262a4
victorloo <ila9970@gmail.com>
2018-03-10 15:41:25 +0800

    create view admin:order edit

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,5 +2,9 @@ class Admin::OrdersController < Admin::AdminController
   def index
     @orders = Order.order(created_at: :desc)
   end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
   
 end
